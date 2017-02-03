a128be998d8f56989e18004a5215df0fcb57c9d5
Dave Hartley <djhartley86@yahoo.com>
2017-02-02 21:35:26 -0800

    mobile playground

---
+++ app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+
+
+/*$( "#burgerIcon" ).click(function() {
+  $( "#burgerIcon" ).animate({
+    opacity: 0.25,
+    height: "toggle"
+  }, 5000, function() {
+});
+})
+*/
+
+$("#burgerIcon, #closeIcon").on("click", function() {
+	$("#burgerIcon").toggleClass("hidden");
+})
+
+
+
+
+
+});
+
+
+
+
+
+
